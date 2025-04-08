b3230541b303eba2241f75a3370f5a2282482a0c
Jader Moura <jader.dev.moura@gmail.com>
2025-04-08 13:09:43 -0300

    Feat: avatars list component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,34 @@
-import { CardComponent, Header } from '@/components'
+import { AvatarList, CardComponent, Header } from '@/components'
 import { Container } from '@mui/material'
+import { currencyConverter } from '@/utils'
 
 function Home() {
+  const mockListData = [
+    {
+      avatar: '/luminare-avatar.png',
+      name: 'Nome Sobrenome 1',
+      subtitle: currencyConverter(1000.0),
+    },
+    {
+      avatar: '/luminare-avatar.png',
+      name: 'Nome Sobrenome 2',
+      subtitle: currencyConverter(2000.0),
+    },
+    {
+      avatar: '/luminare-avatar.png',
+      name: 'Nome Sobrenome 3',
+      subtitle: currencyConverter(3000.0),
+    },
+  ]
+
   return (
     <>
       <Header />
       <Container maxWidth="lg">
         <CardComponent> CARD </CardComponent>
+        <CardComponent>
+          <AvatarList ListData={mockListData} />{' '}
+        </CardComponent>
       </Container>
     </>
   )
