6b9058b4687dd89a1f1a8b7c463575abfd2c4fed
Jader Moura <jader.dev.moura@gmail.com>
2025-04-08 22:00:31 -0300

    Feat: logout flow

--- src/styles/globalStyle.ts
+++ src/styles/globalStyle.ts
@@ -1,5 +1,6 @@
 import { createGlobalStyle } from 'styled-components'
 import { DefaultTheme } from 'styled-components'
+import { pxToRem } from '@/utils'
 
 export const GlobalStyle = createGlobalStyle<{ theme?: DefaultTheme }>`
     body, html {
@@ -23,5 +24,13 @@ export const GlobalStyle = createGlobalStyle<{ theme?: DefaultTheme }>`
 
     table th {
         font-family: 'Cinzel Decorative', serif;
+    }
+
+    .mb-1 {
+        margin-bottom: ${pxToRem(16)};
+    }
+
+    .mb-2 {
+        margin-bottom: ${pxToRem(32)};
     }
 `
