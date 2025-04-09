45821b989e6860570cf40f14faf276b5633fad58
Jader Moura <jader.dev.moura@gmail.com>
2025-04-09 11:27:44 -0300

    Feat: Home page requests

--- src/components/Typographies.tsx
+++ src/components/Typographies.tsx
@@ -6,7 +6,7 @@ export const StyledH1 = styled.h1<TypographiesProps>`
   color: ${(props) => props.color || 'inherit'};
   font-family: 'Cinzel Decorative', serif;
   font-size: ${(props) => pxToRem(props.fontSize || 24)};
-  font-weight: ${(props) => pxToRem(props.fontWeight || 600)};
+  font-weight: ${(props) => props.fontWeight || 600};
   letter-spacing: ${pxToRem(-1)};
   line-height: ${(props) => pxToRem(props.lineHeight || 36)};
 `
@@ -15,7 +15,15 @@ export const StyledH2 = styled.h2<TypographiesProps>`
   color: ${(props) => props.color || 'inherit'};
   font-family: 'Cinzel Decorative', serif;
   font-size: ${(props) => pxToRem(props.fontSize || 16)};
-  font-weight: ${(props) => pxToRem(props.fontWeight || 600)};
+  font-weight: ${(props) => props.fontWeight || 600};
+  line-height: ${(props) => pxToRem(props.lineHeight || 24)};
+`
+
+export const StyledH3 = styled.h2<TypographiesProps>`
+  color: ${(props) => props.color || 'inherit'};
+  font-family: 'Cinzel Decorative', serif;
+  font-size: ${(props) => pxToRem(props.fontSize || 16)};
+  font-weight: ${(props) => props.fontWeight || 600};
   line-height: ${(props) => pxToRem(props.lineHeight || 24)};
 `
 
@@ -23,7 +31,7 @@ export const StyledP = styled.p<TypographiesProps>`
   color: ${(props) => props.color || 'inherit'};
   font-family: 'Lato', sans-serif;
   font-size: ${(props) => pxToRem(props.fontSize || 16)};
-  font-weight: ${(props) => pxToRem(props.fontWeight || 400)};
+  font-weight: ${(props) => props.fontWeight || 400};
   line-height: ${(props) => pxToRem(props.lineHeight || 24)};
 `
 
@@ -31,7 +39,7 @@ export const StyledSpan = styled.span<TypographiesProps>`
   color: ${(props) => props.color || 'inherit'};
   font-family: 'Lato', sans-serif;
   font-size: ${(props) => pxToRem(props.fontSize || 16)};
-  font-weight: ${(props) => pxToRem(props.fontWeight || 400)};
+  font-weight: ${(props) => props.fontWeight || 400};
   line-height: ${(props) => pxToRem(props.lineHeight || 24)};
 `
 
@@ -39,7 +47,7 @@ export const StyledUl = styled.ul<TypographiesProps>`
   color: ${(props) => props.color || 'inherit'};
   font-family: 'Lato', sans-serif;
   font-size: ${(props) => pxToRem(props.fontSize || 16)};
-  font-weight: ${(props) => pxToRem(props.fontWeight || 400)};
+  font-weight: ${(props) => props.fontWeight || 400};
   line-height: ${(props) => pxToRem(props.lineHeight || 24)};
   list-style-position: inside;
   li {
---
+++ src/images/index.ts
@@ -0,0 +1,11 @@
+import fabioImg from './fabio-manoel.png'
+import joaoImg from './joao-medeiros.png'
+import larissaImg from './larissa-lima.png'
+import marinaImg from './marina-ruiz.png'
+
+export const avatars = {
+  'Fábio Manoel': fabioImg,
+  'João Medeiros': joaoImg,
+  'Larissa Lima': larissaImg,
+  'Marina Ruiz': marinaImg,
+}
