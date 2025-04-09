3cf121f81128dae71c56d55ebd71c4b6f110630f
Jader Moura <jader.dev.moura@gmail.com>
2025-04-08 21:17:06 -0300

    Feat: login page requests

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,35 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+  Outlet,
+} from 'react-router-dom'
+import Cookies from 'js-cookie'
 import { Home, Leads, Login, Profile, Registration } from './pages'
 
 function App() {
+  const ProtectedRoute = () => {
+    const checkAuthCookies = Cookies.get('Authorization')
+    if (!checkAuthCookies) {
+      alert('Autentificação necessária!')
+      return <Navigate to="/" replace />
+    }
+
+    return <Outlet />
+  }
+
   return (
     <>
       <Router>
         <Routes>
           <Route path="/" element={<Login />}></Route>
-          <Route path="/home" element={<Home />}></Route>
-          <Route path="/leads" element={<Leads />}></Route>
-          <Route path="/profile" element={<Profile />}></Route>
           <Route path="/registration" element={<Registration />}></Route>
+          <Route element={<ProtectedRoute />}>
+            <Route path="/home" element={<Home />}></Route>
+            <Route path="/leads" element={<Leads />}></Route>
+            <Route path="/profile" element={<Profile />}></Route>
+          </Route>
         </Routes>
       </Router>
     </>
